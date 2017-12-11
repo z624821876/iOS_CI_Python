92ab7a091a1c150fb318d6702f381ca32e9a3a71
yu <yu624821876@163.com>
2017-12-11 17:50:21 +0800

    save

---
+++ AutoPacking/DingHook.py
@@ -0,0 +1,44 @@
+from urllib import request
+import json
+import ssl
+
+Ding_Hook_Url = "***钉钉机器人 webhook_url"
+
+
+class DingHook(object):
+
+    def post_request(self, data):
+
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+        headers = {"Content-Type": "application/json"}
+
+        req = request.Request(url=Ding_Hook_Url, headers=headers)
+        req.get_method = lambda: "POST"
+        http_res = request.urlopen(req, bytes(data, encoding="UTF-8"))
+        content = http_res.read()
+        http_res.close()
+        return content
+
+    def post_qrCode(self, title="", text="", picurl="", messageurl=""):
+        """构建数据"""
+        print("构建钉钉消息数据")
+        data = dict()
+        data["msgtype"] = "link"
+        data["link"] = {}
+        data["link"]["text"] = text
+        data["link"]["title"] = title
+        data["link"]["picUrl"] = picurl
+        data["link"]["messageUrl"] = messageurl
+        data = json.dumps(data)
+        content = self.post_request(data)
+        print("钉钉消息发送成功")
+
+        return content
+
+
+if __name__ == '__main__':
+    dh = DingHook()
+    dh.post_qrCode()
+
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # iOS_CI_Python
 iOS自动打包脚本
+
+
+使用此脚本 需要将python脚本所在脚本放入与项目同等目录。并将dev、master所需要的.mobileprovision文件和打包用的.plist文件一起放入python脚本所在目录
